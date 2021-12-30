f6878d1e6f60c0ff897b0f6e9887d2f31d0f9e94
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2021-12-30 15:01:00 -0700

    demonstrate short int and unsigned int

---
+++ short_int.c
@@ -0,0 +1,16 @@
+/*
+ * Author: Timothy Unkert
+ * Purpose: Demonstrate short int data type
+ * Copyright 2021
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	// short int values -32,768 to 32,767
+	short int age;
+	age = 41;
+	printf("Dean's age at the end of Supernatural is %d.\n", age);
+	return 0;
+}
---
+++ unsigned_int.c
@@ -0,0 +1,14 @@
+/*
+ * Author: Timothy Unkert
+ * Purpose: Demonstrate unsigned integer
+ * Copyright 2021
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	unsigned int castiel_age = 400000000;
+	printf("Castiel is %u years old.\n", castiel_age);
+	return 0;
+}
