d341960ddb9d370771e9c0e94eec98f90b9a8574
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2021-12-29 20:50:02 -0700

    ints, floats, plus formatting

---
+++ numtypes.c
@@ -0,0 +1,15 @@
+/*
+ * Author: Timothy Unkert
+ * Copyright 2021
+ * Purpose: To show the difference between ints and floats
+ */
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Dean's temperature is %2.1f.\n", 98.6);
+  printf("Sam's temperature is %d.\n", 99);
+  printf("Castiel knows that pi is %1.2f.\n", 3.14159265359);
+  return 0;
+}
