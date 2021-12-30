e8ad966f4ec99cfbdaa0b00e2a801a71841764d0
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2021-12-30 15:24:44 -0700

    double data type value

---
+++ double.c
@@ -0,0 +1,15 @@
+/*
+ * Author: Timothy Unkert
+ * Purpose: Demonstrate double
+ * Copyright 2021
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	// values from 2.22 x 10^-308 to 1.79x10^308
+	double earth_age = 4530000000;
+	printf("The earth is about %.0f years old.\n", earth_age);
+	return 0;
+}
