1a94cbb0a36e0eb3e91cf23049a88de49245424a
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2021-12-29 20:42:48 -0700

    demonstrate handling quotes in strings

---
+++ quotes.c
@@ -0,0 +1,14 @@
+/*
+ * Author: Timothy Unkert
+ * Copyright 2021
+ * Purpose: show how to handle quotes within a string
+ */
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Dean says \"hello!\"\n");
+  printf("Castiel also says 'hello!'\n");
+  return 0;
+}
