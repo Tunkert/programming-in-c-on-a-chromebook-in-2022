53e832cb641bf2451827aace9d115edc1ef8f287
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2021-12-29 17:20:19 -0700

    comments file

---
+++ comments.c
@@ -0,0 +1,18 @@
+/*
+ * Written by Timothy Unkert
+ * Copyright 2021
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    // This program describes Tim's thoughts on Supernatural Characters
+    /* Tim really likes Supernatural
+     * he's a bit weird about it but hey
+     * it's a great show. 
+     */
+    printf("Dean is the coolest character in Supernatural.\n");
+    printf("Castiel is also very cool.\n");
+    return 0;
+}
