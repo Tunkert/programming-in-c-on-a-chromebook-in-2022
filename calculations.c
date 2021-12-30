b82bbde4ad95c29bb3709af72dcb0e27014a5296
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2021-12-29 20:30:51 -0700

    show calculations w printf

---
+++ calculations.c
@@ -0,0 +1,16 @@
+/*
+ * Author: Timothy Unkert
+ * Copyright 2021
+ * Purpose: To show calculations can be completed by printf
+ */
+
+#include <stdio.h>
+
+int main()
+{
+  printf("2 + 3 is %d\n", 2+3);
+  printf("2 * 3 is %d\n", 2*3);
+  printf("2 - 3 is %d\n", 2-3);
+  printf("10 / 2 is %d\n", 10 / 2);
+  return 0;
+}
