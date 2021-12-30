dcd594aa129ec6b8e75db65b9545944073f1c3e3
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2021-12-30 15:41:44 -0700

    long long int, mult lines, & under 80

---
+++ multi_lines.c
@@ -0,0 +1,14 @@
+/*
+ * Author: Timothy Unkert
+ * Purpose: demonstrate multi lines methods & concerns
+ * Copyright 2021
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Dean is the coolest character on Supernatural.\nCastiel is also a cool character on Supernatural.\n");
+	// this works but it's over 80 characters
+	return 0;
+}
---
+++ under_80.c
@@ -0,0 +1,14 @@
+/*
+ * Author: Timothy Unkert
+ * Purpose: Demonstrate Methods to keep lines under 80 characters
+ * Copyright 2021
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Dean is a cool character on Supernatural.\nCastiel is also a cool\
+ character on Supernatural.\n");
+	return 0;
+}
