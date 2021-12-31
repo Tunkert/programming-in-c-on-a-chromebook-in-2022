be94be68f951f5f9493ac0575fd701ccfd862652
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2021-12-31 13:33:16 -0700

    getchar 2

---
+++ getchar_2.c
@@ -0,0 +1,16 @@
+/*
+ * Author: Timothy Unkert
+ * Purpose: This code uses an empty getchar to pause the program
+ * Copyright 2021
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Castiel is also pretty cool, press any key and hit return\
+ to learn why.\n");
+    getchar();
+    printf("He's cool because he became \"God\" in season 6.\n");
+    return(0);
+}
