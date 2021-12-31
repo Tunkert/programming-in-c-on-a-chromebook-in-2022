0897aa3a2edebf9629cdc22c3aeb3eea0c027482
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2021-12-31 12:23:44 -0700

    demonstrate char array

---
+++ char_array.c
@@ -0,0 +1,14 @@
+/*
+ * Author: Timothy Unkert
+ * Purpose: Demonstrate character array
+ * Copyright 2021
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    char sup_name[] = "Dean";
+    printf("%s is the coolest character on Supernatural!\n", sup_name);
+    return(0);
+}
