5d8d630fc4409271c9653baf41269d11876754dc
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2022-01-02 07:23:44 -0700

    infinite fl break out w ctrl c

---
+++ infinite_fl.c
@@ -0,0 +1,17 @@
+/*
+ * Author: Timothy Unkert
+ * Purpose: Demonstrate infinite for loop and how to break out of it
+ * use ctrl c to break the loop
+ * also can use break
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    for(;;)
+    {
+        printf("This is an infinite loop! Ahhhhhhhhhhh!\n");
+    }
+    return(0);
+}
