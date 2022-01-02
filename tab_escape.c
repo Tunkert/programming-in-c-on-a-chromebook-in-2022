529b423abdd6179ba156eb47ab32aeb0dff8b008
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2022-01-01 21:31:25 -0700

    demo tab escape

---
+++ tab_escape.c
@@ -0,0 +1,14 @@
+/*
+ * Author: Timothy Unkert
+ * Purpose: Demonstrate tab escape character in C.
+ * Copyright 2021
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("I like Supernatural because it's a fun escape!\n");
+    printf("\tDo you like Supernatural? I like Supernatural!\n");
+    return(0);
+}
