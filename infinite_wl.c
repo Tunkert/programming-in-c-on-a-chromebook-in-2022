2d1d397773d31c1c21c518724eba3cfd27a1c5a0
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2022-01-02 07:26:38 -0700

    infinite wl break out w ctrl c

---
+++ infinite_wl.c
@@ -0,0 +1,17 @@
+/*
+ * Author: Timothy Unkert
+ * Purpose: Demonstrate infinite while loop
+ * Break out with ctrl c
+ * Copyright 2022
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    while(1)
+    {
+        printf("This is an infinite while loop! Ahhhhhhhhhhhh!\n");
+    }
+    return(0);
+}
