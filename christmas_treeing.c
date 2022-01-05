01394d786c8fb67e883bdd9abf763c86282fd4b3
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2022-01-05 10:51:27 -0700

    demo string formatting

---
+++ christmas_treeing.c
@@ -0,0 +1,26 @@
+/*
+ * Author: Timothy Unkert
+ * Purpose: Demonstrate character formatting in a fun way
+ * Copyright 2022
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("%s\n", "a");
+    printf("%2s\n", "b");
+    printf("%3s\n", "c");
+    printf("%4s\n", "d");
+    printf("%3s\n", "c");
+    printf("%2s\n", "b");
+    printf("%s\n", "a");
+    printf("%2s\n", "b");
+    printf("%3s\n", "c");
+    printf("%4s\n", "d");
+    printf("%3s\n", "c");
+    printf("%2s\n", "b");
+    printf("%s\n", "a");
+    return(0);
+}
+    
