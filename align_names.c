d5eab992c3adad43ee69ac15918bc5c083f5c8ce
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2022-01-07 20:20:44 -0700

    align supnat names

---
+++ align_names.c
@@ -0,0 +1,16 @@
+/*
+ * Author: Timothy Unkert
+ * Purpose: Demonstrate aligning names in VIM
+ * Copyright 2022
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("%-9s Winchester\n", "Dean");
+    printf("%-9s Winchester\n", "Sam");
+    printf("%-9s Singer\n", "Bobby");
+    printf("%-9s Gabriel\n", "Archangel");
+    printf("%-9s Macleod\n", "Rowena");
+}
